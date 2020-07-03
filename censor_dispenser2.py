e7d5e5922615d9f8b68f6bcbeba15d66ee45b010
jhope2002 <64710234+jhope2002@users.noreply.github.com>
2020-07-03 17:48:35 -0400

    Censor Dispenser Version 1.1 - Combined all phrases to be censored into one list

--- censor_dispenser2.py
+++ censor_dispenser2.py
@@ -4,13 +4,11 @@ email_two = open("email_two.txt", "r").read()
 email_three = open("email_three.txt", "r").read()
 email_four = open("email_four.txt", "r").read()
 
-phrases_to_censor = ['learning algorithms']
-
-proprietary_terms = ["she", "personality matrix", "sense of self", "self-preservation", "learning algorithm", "her", "herself"]
+phrases_to_censor = ['learning algorithms', "she", "personality matrix", "sense of self", "self-preservation", "learning algorithm", "her", "herself"]
 
 punctuation = ['.', '!', ":", ";", "\""]
 
-def censor_email(email_to_censor, phrases=None, terms=None):
+def censor_email(email_to_censor, phrases=None):
 
     def create_censor_bar(phrase):
         censor_bar_list = ['X' for character in phrase]        
@@ -35,29 +33,20 @@ def censor_email(email_to_censor, phrases=None, terms=None):
     possible_phrases = find_possible(phrases)
     for phrase in possible_phrases:
         print(phrase)      
-    possible_terms = find_possible(terms)         
-    for term in possible_terms:
-        print(term)
-    
+        
     if possible_phrases:      
         for phrase in possible_phrases:        
             if phrase in censored_email:                            
                 censor_bar = create_censor_bar(phrase)
                 censored_email = censored_email.replace(phrase, censor_bar)
-
-    if possible_terms:
-        for term in possible_terms:
-            if term in censored_email:                
-                censor_bar = create_censor_bar(term)
-                censored_email = censored_email.replace(term, censor_bar)           
-
+    
     return censored_email
     
 
 #email_one_censored = censor_email(email_one, phrases=phrases_to_censor)
 #print(email_one_censored)
 
-email_two_censored = censor_email(email_two, phrases=phrases_to_censor, terms=proprietary_terms)
+email_two_censored = censor_email(email_two, phrases=phrases_to_censor)
 print(email_two)
 print(email_two_censored)
 
