4e8e9a6e8c8e255f53ee1d4d988857d781a77160
jhope2002 <64710234+jhope2002@users.noreply.github.com>
2020-07-04 14:05:21 -0400

    Censor Dispenser Version 2.0 - Added Functionality for negative words (Step 4)

--- censor_dispenser2.py
+++ censor_dispenser2.py
@@ -6,9 +6,11 @@ email_four = open("email_four.txt", "r").read()
 
 phrases_to_censor = ['learning algorithms', "she", "personality matrix", "sense of self", "self-preservation", "learning algorithm", "her", "herself"]
 
+negative_words = ["concerned", "behind", "danger", "dangerous", "alarming", "alarmed", "out of control", "help", "unhappy", "bad", "upset", "awful", "broken", "damage", "damaging", "dismal", "distressed", "distressed", "concerning", "horrible", "horribly", "questionable"]
+
 punctuation = ['.', '!', ":", ";", "\""]
 
-def censor_email(email_to_censor, phrases=None):
+def censor_email(email_to_censor, phrases=None, words=None):
 
     def create_censor_bar(phrase):
         censor_bar_list = ['X' for character in phrase]        
@@ -33,22 +35,42 @@ def censor_email(email_to_censor, phrases=None):
     possible_phrases = find_possible(phrases)
     for phrase in possible_phrases:
         print(phrase)      
+    possible_words = find_possible(words)
+    for word in possible_words:
+        print(word)
         
     if possible_phrases:      
         for phrase in possible_phrases:        
             if phrase in censored_email:                            
                 censor_bar = create_censor_bar(phrase)
                 censored_email = censored_email.replace(phrase, censor_bar)
+
+    if possible_words:
+        counter = 0
+        for word in possible_words:            
+            if word in censored_email:
+                counter += 1
+                print("Negative word found!  >> " + word)
+                print("Counter is now >> " + str(counter))
+                if counter > 2:
+                    censor_bar = create_censor_bar(word)
+                    censored_email = censored_email.replace(word, censor_bar)
     
     return censored_email
     
 
 #email_one_censored = censor_email(email_one, phrases=phrases_to_censor)
+#print(eamil_one)
 #print(email_one_censored)
 
-email_two_censored = censor_email(email_two, phrases=phrases_to_censor)
-print(email_two)
-print(email_two_censored)
+#email_two_censored = censor_email(email_two, phrases=phrases_to_censor)
+#print(email_two)
+#print(email_two_censored)
+
+email_three_censored = censor_email(email_three, phrases=phrases_to_censor, words=negative_words)
+print(email_three)
+print(email_three_censored)
+
 
 
 
